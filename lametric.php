3e897cbf8a75442bb77b041d6e0b18522159b379
DamienLGD <legendre.dam@gmail.com>
2017-06-05 22:52:36 +0200

    1.0
    Script lametric for eedomus

---
+++ lametric.php
@@ -0,0 +1,48 @@
+
+<?php
+$url = "xxxx";
+$token = "xxx";
+
+if(isset($_GET['valeur']))
+{
+    $valeur=$_GET['valeur'];
+}
+if(isset($_GET['icon']))
+{
+    $icon=$_GET['icon'];
+}
+
+$frames = array(
+    "frames" => array(
+            array(
+                "index" => 0,
+                "icon" => "$icon",
+                "text" => "$valeur"
+            ),
+)   
+);
+
+$curl = curl_init();
+
+$headers = array(
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "X-Access-Token: $token", 
+    "Cache-Control: no-cache", 
+);
+
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_URL, $url); 
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); 
+curl_setopt($curl, CURLOPT_POST, 1); 
+curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($frames)); 
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
+
+
+$response = curl_exec($curl);
+curl_close($curl);
+
+?>
