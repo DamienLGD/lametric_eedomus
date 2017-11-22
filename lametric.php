5c0db354dc2663139fac72ee8ce2bd7003e87233
DamienLGD <legendre.dam@gmail.com>
2017-11-22 12:08:17 +0100

    Update lametric.php

--- lametric.php
+++ lametric.php
@@ -34,13 +34,10 @@ $headers = array(
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($curl, CURLOPT_URL, $url); 
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
-curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); 
 curl_setopt($curl, CURLOPT_POST, 1); 
 curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($frames)); 
 curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, 0);
-
 
 $response = curl_exec($curl);
 curl_close($curl);
